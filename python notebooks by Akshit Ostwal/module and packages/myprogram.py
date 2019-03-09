10c916e3a7cf72d40b3729a470883aef23df11df
Akshit Ostwal <akshitostwal@gmail.com>
2019-03-09 16:05:39 +0530

    Create myprogram.py

---
+++ python notebooks by Akshit Ostwal/module and packages/myprogram.py
@@ -0,0 +1,7 @@
+from mymodule import my_func
+from MyMainPackage import some_main_script
+from MyMainPackage.SubPackage import mysubscript
+
+some_main_script.report_main()
+mysubscript.sub_report()
+my_func()
